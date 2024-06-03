728841cb2c85c35a20bf8913678fed45dcdc8eae
Jordi Ferrer Orri <9082687+jordiferrero@users.noreply.github.com>
2024-06-03 18:46:47 +0200

    first commit

---
+++ readme.md
@@ -0,0 +1,37 @@
+
+## Install and launch neo4j
+
+To launch Neo4j locally, first ensure you have docker installed. Then, you can launch the database with the following docker command:
+
+```bash
+docker run \
+    -p 7474:7474 -p 7687:7687 \
+    -v $PWD/data:/data -v $PWD/plugins:/plugins \
+    --name neo4j-apoc \
+    -e NEO4J_apoc_export_file_enabled=true \
+    -e NEO4J_apoc_import_file_enabled=true \
+    -e NEO4J_apoc_import_file_use__neo4j__config=true \
+    -e NEO4JLABS_PLUGINS=\[\"apoc\"\] \
+    neo4j:latest
+```
+
+From here, you can open the db at [http://localhost:7474/](http://localhost:7474/). On this page, you will be asked to sign in. Use the default username/password of `neo4j` and `neo4j`.
+
+Once you login for the first time, you will be asked to change the password. I changed it to `12345678`.
+
+After this, you are ready to create your first property graph!
+
+
+# Install an env to run langchain
+
+Install dependencies
+```bash
+pip install --upgrade  -r requirements.txt
+```
+
+Then run `main.py`.
+
+# References
+
+Project adapted from https://python.langchain.com/v0.1/docs/use_cases/graph/constructing/
+Also inspired by https://colab.research.google.com/github/run-llama/llama_index/blob/main/docs/docs/examples/property_graph/property_graph_advanced.ipynb#scrollTo=EL6tYUI14S3D
---
+++ src/main.py
@@ -0,0 +1,47 @@
+import os
+from dotenv import load_dotenv
+
+from langchain_community.graphs import Neo4jGraph
+from langchain_experimental.graph_transformers import LLMGraphTransformer
+from langchain_openai import ChatOpenAI
+
+load_dotenv(".env")
+
+os.environ["NEO4J_URI"] = os.environ.get("NEO4J_URI")
+os.environ["NEO4J_USERNAME"] = os.environ.get("NEO4J_USERNAME")
+os.environ["NEO4J_PASSWORD"] = os.environ.get("NEO4J_PASSWORD")
+
+os.environ["OPENAI_API_KEY"] = os.environ.get("OPENAI_API_KEY")
+os.environ["LANGCHAIN_API_KEY"] = os.environ.get("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_PROJECT"] = os.environ.get("LANGCHAIN_PROJECT")
+os.environ["LANGCHAIN_TRACING_V2"] = os.environ.get("LANGCHAIN_TRACING_V2")
+
+graph = Neo4jGraph()
+
+
+llm = ChatOpenAI(temperature=0, model_name="gpt-4-turbo")
+
+llm_transformer = LLMGraphTransformer(llm=llm)
+
+from langchain_core.documents import Document
+
+text = """
+Marie Curie, born in 1867, was a Polish and naturalised-French physicist and chemist who conducted pioneering research on radioactivity.
+She was the first woman to win a Nobel Prize, the first person to win a Nobel Prize twice, and the only person to win a Nobel Prize in two scientific fields.
+Her husband, Pierre Curie, was a co-winner of her first Nobel Prize, making them the first-ever married couple to win the Nobel Prize and launching the Curie family legacy of five Nobel Prizes.
+She was, in 1906, the first woman to become a professor at the University of Paris.
+"""
+documents = [Document(page_content=text)]
+
+llm_transformer_props = LLMGraphTransformer(
+    llm=llm,
+    allowed_nodes=["Person", "Country", "Organization"],
+    allowed_relationships=["NATIONALITY", "LOCATED_IN", "WORKED_AT", "SPOUSE"],
+    node_properties=["born_year"],
+)
+graph_documents_props = llm_transformer_props.convert_to_graph_documents(documents)
+print(f"Nodes:{graph_documents_props[0].nodes}")
+print(f"Relationships:{graph_documents_props[0].relationships}")
+
+graph.add_graph_documents(graph_documents_props)
+print("Neo4j graph updated.")
